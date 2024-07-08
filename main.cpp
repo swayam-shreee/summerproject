4b8223b71b9e84668a43446227e0ff3b382cf4f5
spatrascu <143308472+spatrascu@users.noreply.github.com>
2024-07-07 22:36:05 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "map.hpp"
+int main() {
+    //16 rooms -> create 16 rooms
+    // intialize a 4*4 2-d vector
+    // 4 rows and 4 columns
+    std::vector<std::vector<Room> > map (4, std::vector<Room>(4));
+    
+
+
+
+     //initialize each room
+     map[0][0] = Room("MysteriousForest",  );
+     map[0][1] =
+     map[0][2] =
+     map[0][3] =
+     map[1][0] =
+     map[1][1] =
+     map[1][2] =
+     map[1][3] =
+     map[2][0] =
+     map[2][1] =
+     map[2][2] =
+     map[2][3] =
+     map[3][0] =
+     map[3][1] =
+     map[3][2] =
+     map[3][3] =
+}
