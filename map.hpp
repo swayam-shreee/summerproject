3e238194b32a26dc312dccb2abe1d21f21d27d10
spatrascu <143308472+spatrascu@users.noreply.github.com>
2024-07-07 22:36:38 -0700

    Create map.hpp

---
+++ map.hpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+class Room {
+    private:
+    std::string roomName;
+    std::string roomDescription;
+    std::vector<std::string> items;
+    std::vector<std::string> directions;
+    
+    public:
+     Room(std::string name, std::string description){
+        this->roomName = name;
+        this->roomDescription = description;
+     }
+     
+     std::string getRoomName(){
+        return roomName;
+     }
+
+    void setRoomName(std::string name){
+        this->roomName = name;
+    }
+
+    std::string getRoomDescription(){
+        return roomDescription;
+    }
+     
+
+
+};
